c5391becbacc56fb81be1ac2a7973fee1cbf9ca4
Stephen Olayemi <olayemistephen007@gmail.com>
2022-01-07 13:02:44 +0100

    changes in progress

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Model
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Publisher { get; set; }
+
+        [Required]
+        public string NaCostme { get; set; }
+    }
+}
