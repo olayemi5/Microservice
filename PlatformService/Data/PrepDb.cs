e77333cc56a08c10c167e33cc26e72a65baf87fc
Stephen Olayemi <olayemistephen007@gmail.com>
2022-01-10 16:38:21 +0100

    Adding env changes to select database

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,53 +1,72 @@
 using PlatformService.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool isProd)
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), isProd);
             }
         }
 
-        private static void SeedData(AppDbContext context)
+        private static void SeedData(AppDbContext context, bool isProd)
         {
-            if (!context.Platforms.Any())
+            if (isProd)
             {
-                Console.WriteLine("---> Seeding Data ...");
+                Console.WriteLine("--> Attempting Migration");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine($"--> COuld not run migration {ex.Message}");
+                }
 
-                context.Platforms.AddRange(
-                    new Platform()
-                    {
-                        Id = 1,
-                        Name = "Dotnet",
-                        Publisher = "Microsoft",
-                        Cost = "Free"
-                    },
-                    new Platform()
-                    {
-                        Id = 2,
-                        Name = "Sql Server Express",
-                        Publisher = "Microsoft",
-                        Cost = "Free"
-                    },
-                    new Platform()
-                    {
-                        Id = 3,
-                        Name = "Kubernetes",
-                        Publisher = "MiCLoud Native Computing Foundation",
-                        Cost = "Free"
-                    }
-                );
-
-                context.SaveChanges();
             }
             else
             {
-                Console.WriteLine("---> We alreay have data");
+                if (!context.Platforms.Any())
+                {
+                    Console.WriteLine("---> Seeding Data ...");
+
+                    context.Platforms.AddRange(
+                        new Platform()
+                        {
+                            Id = 1,
+                            Name = "Dotnet",
+                            Publisher = "Microsoft",
+                            Cost = "Free"
+                        },
+                        new Platform()
+                        {
+                            Id = 2,
+                            Name = "Sql Server Express",
+                            Publisher = "Microsoft",
+                            Cost = "Free"
+                        },
+                        new Platform()
+                        {
+                            Id = 3,
+                            Name = "Kubernetes",
+                            Publisher = "MiCLoud Native Computing Foundation",
+                            Cost = "Free"
+                        }
+                    );
+
+                    context.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("---> We alreay have data");
+                }
             }
+
+
         }
     }
 }
